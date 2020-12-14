fec11b5318406da59f4ed3a66612d2cf7864f206
Max Becker <max.becker@eng-its.de>
2020-12-14 15:33:50 +0100

    initial import

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1630e2fef6999950d29d159f5626c47d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be6d70b7-b855-420e-a536-0b2e4f29830f'
+};
